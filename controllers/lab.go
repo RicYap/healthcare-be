0d893534edc26fa311e5c71665cd7c041af9c36a
ricky_yap <aryatamayaputera@gmail.com>
2025-06-19 16:59:54 +0700

    format input json

--- controllers/lab.go
+++ controllers/lab.go
@@ -1,8 +1,8 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
-	"time"
 
 	"healthcare-be/config"
 	"healthcare-be/models"
@@ -11,35 +11,24 @@ import (
 	"github.com/google/uuid"
 )
 
-type LabInput struct {
-	UserId           uuid.UUID `json:"user_id"`
-	Date             time.Time `json:"date"`
-	Glucose          float64   `json:"glucose"`
-	CholesterolTotal float64   `json:"cholesterol_total"`
-	LDL              float64   `json:"ldl"`
-	HDL              float64   `json:"hdl"`
-	Systolic         int       `json:"systolic"`
-	Diastolic        int       `json:"diastolic"`
-}
-
 func AddLabResult(c *gin.Context) {
-	var input LabInput
+	var input models.LabInput
 
 	if err := c.ShouldBindJSON(&input); err != nil {
+		log.Println("err", err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
 		return
 	}
 
-	// userID := c.MustGet("userId").(uuid.UUID)
 	result := models.LabResult{
-		UserID:           input.UserId,
+		UserID:           input.UserID,
 		Date:             input.Date,
-		Glucose:          input.Glucose,
-		CholesterolTotal: input.CholesterolTotal,
-		LDL:              input.LDL,
-		HDL:              input.HDL,
-		Systolic:         input.Systolic,
-		Diastolic:        input.Diastolic,
+		Glucose:          input.Results.Glucose,
+		CholesterolTotal: input.Results.Cholesterol.Total,
+		LDL:              input.Results.Cholesterol.LDL,
+		HDL:              input.Results.Cholesterol.HDL,
+		Systolic:         input.Results.BloodPressure.Systolic,
+		Diastolic:        input.Results.BloodPressure.Diastolic,
 	}
 
 	config.DB.Create(&result)
@@ -50,5 +39,6 @@ func GetLabResults(c *gin.Context) {
 	userID := c.MustGet("userId").(uuid.UUID)
 	var results []models.LabResult
 	config.DB.Where("user_id = ?", userID).Order("date desc").Find(&results)
+
 	c.JSON(http.StatusOK, results)
 }
--- models/lab.go
+++ models/lab.go
@@ -14,8 +14,32 @@ type LabResult struct {
 	CholesterolTotal float64   `gorm:"type:float"`
 	LDL              float64   `gorm:"type:float"`
 	HDL              float64   `gorm:"type:float"`
-	Systolic         int       `gorm:"type:int"`
-	Diastolic        int       `gorm:"type:int"`
+	Systolic         float64   `gorm:"type:int"`
+	Diastolic        float64   `gorm:"type:int"`
+}
+
+type LabInput struct {
+	ID      uuid.UUID `json:"id"`
+	UserID  uuid.UUID `json:"userId"`
+	Date    time.Time `json:"date"`
+	Results Lab       `json:"results"`
+}
+
+type Lab struct {
+	Glucose       float64         `json:"glucose"`
+	Cholesterol   CholesterolInfo `json:"cholesterol"`
+	BloodPressure BloodPressure   `json:"bloodPressure"`
+}
+
+type CholesterolInfo struct {
+	Total float64 `json:"total"`
+	LDL   float64 `json:"ldl"`
+	HDL   float64 `json:"hdl"`
+}
+
+type BloodPressure struct {
+	Systolic  float64 `json:"systolic"`
+	Diastolic float64 `json:"diastolic"`
 }
 
 func (l *LabResult) BeforeCreate(tx *gorm.DB) (err error) {
